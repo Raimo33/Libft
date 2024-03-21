fed5dcc3cc52cd088a1565d6fbc9f15ca40cc0ef
Raimo <104778891+Raimo33@users.noreply.github.com>
2024-03-21 18:15:15 +0100

    Update ft_lstclear_bonus.c

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -14,11 +14,12 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
-	if (*lst == NULL || del == NULL || lst == NULL)
+	if (*lst == NULL || lst == NULL)
 		return ;
 	if ((*lst)->next != NULL)
 		ft_lstclear(&(*lst)->next, del);
-	del((*lst)->content);
+	if (del)
+		del((*lst)->content);
 	free(*lst);
 	*lst = NULL;
 }
