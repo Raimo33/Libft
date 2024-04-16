d420f32a531d03cb8f9be8cf5879441914d59d12
Raimo <104778891+Raimo33@users.noreply.github.com>
2024-04-16 14:45:52 +0200

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -14,8 +14,10 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	if (n == 0 || !s1 || !s2)
+	if (n == 0)
 		return (0);
+	else if (!s1 || !s2)
+		return (1);
 	while ((*s1 == *s2) && n - 1 > 0 && *s1 != '\0' && *s2 != '\0')
 	{
 		s1++;
